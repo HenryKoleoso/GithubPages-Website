dbb309a0c2c8492f96ee294e5c15bcd1a33f4cbb
Henry Koleoso <hkoleoso@spartaglobal.com>
2018-02-21 13:27:24 +0000

    now hosted wtih docker and jekyll locally

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "What's Jekyll?",
+        "excerpt":"[Jekyll](http://jekyllrb.com) is a static site generator, an open-source tool for creating simple yet powerful websites of all shapes and sizes. From [the project's readme](https://github.com/mojombo/jekyll/blob/master/README.markdown): > Jekyll is a simple, blog aware, static site generator. It takes a template directory [...] and spits out a complete, static website suitable for serving...","categories": ["hyde"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/hyde/whats-jekyll/",
+        "teaser":null},{
+        "title": "Example content",
+        "excerpt":"Howdy! This is an example blog post that shows several types of HTML content supported in this theme.Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. *Aenean eu leo quam.* Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus...","categories": ["hyde"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/hyde/example-content/",
+        "teaser":null},{
+        "title": "Introducing Hyde",
+        "excerpt":"Hyde is a brazen two-column [Jekyll](http://jekyllrb.com) theme that pairs a prominent sidebar with uncomplicated content. It's based on [Poole](http://getpoole.com), the Jekyll butler.### Built on PoolePoole is the Jekyll Butler, serving as an upstanding and effective foundation for Jekyll themes by [@mdo](https://twitter.com/mdo). Poole, and every theme built on it (like Hyde...","categories": ["hyde"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/hyde/introducing-hyde/",
+        "teaser":null}]
